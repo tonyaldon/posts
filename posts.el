4df9b73c11ce04726297eb43f0fe3d11c5adc621
Tony Aldon <tony@tonyaldon.com>
2023-04-11 19:20:19 +0200

    replace `one-default-doc` -> `posts-one-default-doc`

--- posts.el
+++ posts.el
@@ -1,6 +1,13 @@
 (global-set-key
  (kbd "C-<f1>")
  (lambda () (interactive) (switch-to-buffer "posts.org")))
+(global-set-key
+ (kbd "C-<f2>")
+ (lambda () (interactive)
+   (with-current-buffer "posts.el"
+     (eval-buffer))
+   (with-current-buffer "posts.org"
+     (one-build-only-html))))
 
 (defun posts-rev-link-store ()
   ""
@@ -68,3 +75,79 @@ See `org-rev-open'."
 
 (org-link-set-parameters "help"
 			                   :export #'posts-one-ox-link-help)
+
+
+(defun posts-one-default-doc (page-tree pages global)
+  ""
+  (let* ((title (org-element-property :raw-value page-tree))
+         (path (org-element-property :CUSTOM_ID page-tree))
+         (content (org-export-data-with-backend
+                   (org-element-contents page-tree)
+                   'one nil))
+         (website-name (one-default-website-name pages))
+         (pages-list (one-default-pages pages))
+         (headlines (cdr (one-default-list-headlines page-tree)))
+         (toc (when headlines
+                `(:div/toc
+                  (:div
+                   (:div "Table of content")
+                   (:div ,(one-default-toc headlines))))))
+         (nav (one-default-nav path pages))
+         (date (substring path 1 11))
+         (reddit-post (org-element-property :REDDIT_POST page-tree))
+         (commit-emacs (org-element-property :COMMIT_EMACS page-tree))
+         (commit-org-mode (org-element-property :COMMIT_ORG_MODE page-tree)))
+    (jack-html
+     "<!DOCTYPE html>"
+     `(:html
+       (:head
+        (:meta (@ :name "viewport" :content "width=device-width,initial-scale=1"))
+        (:link (@ :rel "stylesheet" :type "text/css" :href "/one.css"))
+        (:title ,title))
+       (:body
+        ;; sidebar-left and sidebar-main are for small devices
+        (:div/sidebar-left (@ :onclick "followSidebarLink()")
+         ;; (:div (:div ,website-name))
+         (:div (:div "Pages"))
+         ,pages-list)
+        (:div/sidebar-main)
+        (:div/header-doc
+         (:svg/hamburger (@ :viewBox "0 0 24 24" :onclick "sidebarShow()")
+          (:path (@ :d "M21,6H3V5h18V6z M21,11H3v1h18V11z M21,17H3v1h18V17z")))
+         (:a (@ :href "/") ,website-name))
+        (:div/content-doc
+         (:div/sidebar ,pages-list)
+         (:article
+          (:div/title (:h1 ,title))
+          (:div/meta-info
+           (:div ,date)
+           "/"
+           (:div "Tony Aldon")
+           "/"
+           (:div (:a (@ :href ,reddit-post) "comment on reddit"))
+           ,(cond
+             ((and commit-emacs commit-org-mode)
+              (list "/" `(:div "emacs revision: " ,(substring commit-emacs 0 12))
+                    "/" `(:div "org-mode revision: " ,(substring commit-org-mode 0 12))))
+             (commit-emacs
+              (list "/" `(:div "emacs revision: " ,(substring commit-emacs 0 12))))
+             (commit-org-mode
+              (list "/" `(:div "org-mode revision: " ,(substring commit-org-mode 0 12))))))
+          ,toc
+          ,content
+          ,nav)))
+       (:script "
+function sidebarShow() {
+  if (window.innerWidth < 481)
+    document.getElementById('sidebar-left').style.width = '75vw';
+  else {
+    document.getElementById('sidebar-left').style.width = 'min(300px, 34vw)';
+  }
+  document.getElementById('sidebar-main').setAttribute('onclick', 'sidebarHide()');
+  document.getElementById('sidebar-main').style.display = 'block';
+}
+function sidebarHide() {
+  document.getElementById('sidebar-left').style.width = '0';
+  document.getElementById('sidebar-main').style.display = 'none';
+}
+")))))
