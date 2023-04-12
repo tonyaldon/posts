ab2813776596f572ee81c606a1f022a14bfb8f11
Tony Aldon <tony@tonyaldon.com>
2023-04-12 08:25:44 +0200

    organize `posts.el`

--- posts.el
+++ posts.el
@@ -1,81 +1,3 @@
-(global-set-key
- (kbd "C-<f1>")
- (lambda () (interactive) (switch-to-buffer "posts.org")))
-(global-set-key
- (kbd "C-<f2>")
- (lambda () (interactive)
-   (with-current-buffer "posts.el"
-     (eval-buffer))
-   (with-current-buffer "posts.org"
-     (one-build-only-html))))
-
-(defun posts-rev-link-store ()
-  ""
-  (interactive)
-  (let* ((store-data (org-rev-store-data))
-         (description-name
-          (rich-atom-code-name (plist-get store-data :line-content)
-                               (plist-get store-data :major-mode)))
-         (link-name (format (plist-get store-data :link-format) description-name)))
-    (kill-new link-name)))
-
-;; (key-chord-define-global "//" 'posts-rev-link-store)
-
-(defun posts-one-ox-link-org-rev (link description type info)
-  "Export `org-rev' links.
-
-See `org-rev-open'."
-  (seq-let (rev repo file line) (split-string link ":")
-    (let (href)
-      (if (string= repo "emacs")
-          (setq href
-                (concat
-                 "https://github.com/emacs-mirror/emacs/blob/"
-                 rev "/"
-                 file (or (and line (concat "#L" line)) "")))
-        (setq href
-              (concat
-               "https://git.sr.ht/~bzg/org-mode/tree/"
-               rev "/item/"
-               file (or (and line (concat "#L" line)) ""))))
-      (jack-html
-       `(:a (@ :href ,href) ,description)
-       ))))
-
-;; [[rev:af6f1298b6f613678ba5ccf9592412872743fe54:org-mode:lisp/ob-awk.el:41][org-babel-tangle-lang-exts]]
-;; https://git.sr.ht/~bzg/org-mode/tree/af6f1298b6f613678ba5ccf9592412872743fe54/item/lisp/ob-awk.el#L41
-
-;; (split-string "rev:repo:file:line" ":") ; ("rev" "repo" "file" "line")
-;; (posts-one-ox-link-org-rev "rev:repo:file:line" "description" nil nil)
-
-;; https://github.com/emacs-mirror/emacs/blob/cb4f4dd89131e5a8956c788fee7ede65f13b2a69/src/alloc.c#L8214
-;; (posts-one-ox-link-org-rev "cb4f4dd89131e5a8956c788fee7ede65f13b2a69:emacs:src/alloc.c:8214" "#ifdef HAVE_PGTK" nil nil)
-
-(posts-one-ox-link-org-rev "af6f1298b6f613678ba5ccf9592412872743fe54:org-mode:lisp/ob-awk.el:41"
-                           "org-babel-tangle-lang-exts" nil nil)
-;; "<a href=\"https://git.sr.ht/~bzg/org-mode/tree/af6f1298b6f613678ba5ccf9592412872743fe54/item/lisp/ob-awk.el#L41\">org-babel-tangle-lang-exts</a>"
-
-(posts-one-ox-link-org-rev "cb4f4dd89131e5a8956c788fee7ede65f13b2a69:emacs:lisp/ob-awk.el:41"
-                           "#ifdef HAVE_PGTK" nil nil)
-;; "<a href=\"https://github.com/emacs-mirror/emacs/blob/cb4f4dd89131e5a8956c788fee7ede65f13b2a69/src/alloc.c#L8214\">#ifdef HAVE_PGTK</a>"
-
-(org-link-set-parameters "rev"
-			                   :export #'posts-one-ox-link-org-rev)
-
-(defun posts-one-ox-link-info (link description type info)
-  "Export builtin org info links. "
-  (jack-html `(:span ,link)))
-
-(org-link-set-parameters "info"
-			                   :export #'posts-one-ox-link-info)
-
-(defun posts-one-ox-link-help (link description type info)
-  "Export builtin org help links. "
-  (jack-html `(:span ,link)))
-
-(org-link-set-parameters "help"
-			                   :export #'posts-one-ox-link-help)
-
 
 (defun posts-one-default-doc (page-tree pages global)
   ""
@@ -107,7 +29,6 @@ See `org-rev-open'."
        (:body
         ;; sidebar-left and sidebar-main are for small devices
         (:div/sidebar-left (@ :onclick "followSidebarLink()")
-         ;; (:div (:div ,website-name))
          (:div (:div "Pages"))
          ,pages-list)
         (:div/sidebar-main)
@@ -149,3 +70,75 @@ function sidebarHide() {
   document.getElementById('sidebar-main').style.display = 'none';
 }
 ")))))
+
+;;; export links
+
+(defun posts-one-ox-link-org-rev (link description type info)
+  "Export `org-rev' links.
+
+See `org-rev-open'."
+  (seq-let (rev repo file line) (split-string link ":")
+    (let (href)
+      (if (string= repo "emacs")
+          (setq href
+                (concat
+                 "https://github.com/emacs-mirror/emacs/blob/"
+                 rev "/"
+                 file (or (and line (concat "#L" line)) "")))
+        (setq href
+              (concat
+               "https://git.sr.ht/~bzg/org-mode/tree/"
+               rev "/item/"
+               file (or (and line (concat "#L" line)) ""))))
+      (jack-html
+       `(:a (@ :href ,href) ,description)))))
+
+
+;; (posts-one-ox-link-org-rev "af6f1298b6f613678ba5ccf9592412872743fe54:org-mode:lisp/ob-awk.el:41"
+;;                            "org-babel-tangle-lang-exts" nil nil)
+;; ;; "<a href=\"https://git.sr.ht/~bzg/org-mode/tree/af6f1298b6f613678ba5ccf9592412872743fe54/item/lisp/ob-awk.el#L41\">org-babel-tangle-lang-exts</a>"
+;; (posts-one-ox-link-org-rev "cb4f4dd89131e5a8956c788fee7ede65f13b2a69:emacs:lisp/ob-awk.el:41"
+;;                            "#ifdef HAVE_PGTK" nil nil)
+;; ;; "<a href=\"https://github.com/emacs-mirror/emacs/blob/cb4f4dd89131e5a8956c788fee7ede65f13b2a69/src/alloc.c#L8214\">#ifdef HAVE_PGTK</a>"
+
+(org-link-set-parameters "rev"
+			                   :export #'posts-one-ox-link-org-rev)
+
+(defun posts-one-ox-link-info (link description type info)
+  "Export builtin org info links. "
+  (jack-html `(:span ,link)))
+
+(org-link-set-parameters "info"
+			                   :export #'posts-one-ox-link-info)
+
+(defun posts-one-ox-link-help (link description type info)
+  "Export builtin org help links. "
+  (jack-html `(:span ,link)))
+
+(org-link-set-parameters "help"
+			                   :export #'posts-one-ox-link-help)
+
+;;; comments
+
+;; (global-set-key
+;;  (kbd "C-<f1>")
+;;  (lambda () (interactive) (switch-to-buffer "posts.org")))
+;; (global-set-key
+;;  (kbd "C-<f2>")
+;;  (lambda () (interactive)
+;;    (with-current-buffer "posts.el"
+;;      (eval-buffer))
+;;    (with-current-buffer "posts.org"
+;;      (one-build-only-html))))
+
+;; (defun posts-rev-link-store ()
+;;   ""
+;;   (interactive)
+;;   (let* ((store-data (org-rev-store-data))
+;;          (description-name
+;;           (rich-atom-code-name (plist-get store-data :line-content)
+;;                                (plist-get store-data :major-mode)))
+;;          (link-name (format (plist-get store-data :link-format) description-name)))
+;;     (kill-new link-name)))
+;;
+;; ;; (key-chord-define-global "//" 'posts-rev-link-store)
